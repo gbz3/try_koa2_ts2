8217673f77a649c10ef299d523ccc12a72dd9c42
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-03-13 13:16:44 +0900

    log4js 導入

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import Koa from 'koa'
 import http2 from 'http2'
 import fs from 'fs'
 
+import { buildLogger } from './modules/logger'
+
 // Koa2 サーバ初期設定
 const app = new Koa()
+const appLogger = buildLogger('APP')
 
 // ミドルウェア設定
 app.use(async (ctx) => {
@@ -17,3 +20,4 @@ const options = {
   cert: fs.readFileSync('ssc/server.crt')
 }
 http2.createSecureServer(options, app.callback()).listen(port)
+appLogger.info(`listening on port ${port}`)
