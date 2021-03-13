53a9eed64097464912ffd7fe4c4079b097bcd007
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-03-13 12:56:18 +0900

    HTTP/2 導入

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Koa from 'koa'
+import http2 from 'http2'
+import fs from 'fs'
 
 // Koa2 サーバ初期設定
 const app = new Koa()
@@ -10,4 +12,8 @@ app.use(async (ctx) => {
 
 // サーバ起動
 const port = process.env.PORT || 443
-app.listen(port)
+const options = {
+  key: fs.readFileSync('ssc/server.key'),
+  cert: fs.readFileSync('ssc/server.crt')
+}
+http2.createSecureServer(options, app.callback()).listen(port)
