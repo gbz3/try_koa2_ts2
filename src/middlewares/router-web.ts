e9c085997d3e9f20abcdcdc2ab1347498c99077b
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-03-14 09:38:56 +0900

    環境変数で prefix 有無を指定

--- src/middlewares/router-web.ts
+++ src/middlewares/router-web.ts
@@ -3,11 +3,13 @@ import Koa from 'koa'
 import compose from 'koa-compose'
 
 // see: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/36161#issuecomment-571295417
-const RouterWeb = new Router<Koa.DefaultState, Koa.Context>()
-  .get('/', async (ctx: Koa.Context) => {
-    ctx.body = "koa app."
+const RouterWebPub = new Router<Koa.DefaultState, Koa.Context>('PUB' in process.env? { prefix: `/${process.env.PUB}` }: {})
+  .get('/', async (ctx: Koa.Context) => ctx.body = `koa app. (PUB=${process.env.PUB})`)
 
-    ctx.logger.info(`PUB=${process.env.PUB} PRI=${process.env.PRI}`)
-  })
+const RouterWebPri = new Router<Koa.DefaultState, Koa.Context>('PRI' in process.env? { prefix: `/${process.env.PRI}` }: {})
+  .get('/', async (ctx: Koa.Context) => ctx.body = `koa app. (PRI=${process.env.PRI})`)
 
-export const mwRouterWeb = compose([RouterWeb.routes(), RouterWeb.allowedMethods()])
+export const mwRouterWeb = compose(
+  [...('PUB' in process.env? [RouterWebPub]: []), ...('PRI' in process.env? [RouterWebPri]: [])]
+  .flatMap(e => [e.routes(), e.allowedMethods()])
+)
