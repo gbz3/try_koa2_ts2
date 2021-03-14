f3f62b5fba073f5be2269deb5f6941424574f7ce
gbz3 <1808483+gbz3@users.noreply.github.com>
2021-03-14 15:53:40 +0900

    Skyway 接続まで

--- src-wp/index.ts
+++ src-wp/index.ts
@@ -1,5 +1,25 @@
 import { hello } from './modules/sub'
+import Peer from 'skyway-js'
 import 'bootstrap'
 import './index.scss'
 
 hello()
+
+const referenceTo = <T extends HTMLElement>(id: string) => {
+  const idRef = document.getElementById(id)
+  if (idRef == null) throw new Error(`${id} not found.`)
+  return idRef as T
+}
+
+const myId = referenceTo<HTMLInputElement>('my-id')
+const peerId = referenceTo<HTMLInputElement>('peer-id')
+
+// 初期化
+;(async () => {
+  // Skyway 接続
+  const skywayKeyRes = await fetch('skyway-key')
+  const peer = new Peer({ key: await skywayKeyRes.text(), debug: 3 })
+  // Skyway 接続成功
+  peer.on('open', () => myId.value = peer.id)
+
+})()
